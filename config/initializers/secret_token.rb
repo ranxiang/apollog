56188df0eb67be16b4823dda038dfc2256f68717
Ran Xiang <ranxiang@gmail.com>
2011-06-25 12:54:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apollog::Application.config.secret_token = '3fdc6bb3955f421eb3a1735720ed64231d167fe9abe793a785f06969e7849e37fadd1fa0608bd9fd9d055b3d385c10321dff2d4100c96844e740e9dc867b52a8'
